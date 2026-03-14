740b97ae8ffe1f168bdad02d4698ae9de1fb23ea
vicky <panchalvicky501@gmail.com>
2026-03-14 12:50:36 +0530

    update app file

--- Backends/app/main.py
+++ Backends/app/main.py
@@ -11,9 +11,11 @@ origins = [
     "http://localhost:5173"
 ]
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
